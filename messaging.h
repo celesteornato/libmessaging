05e4ac9094d287b269e7c5a6da25568688653643
Céleste Ornato <celeste@ornato.com>
2025-06-27 19:34:54 +0200

    improved semantics

--- messaging.h
+++ messaging.h
@@ -4,8 +4,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void send_data(const char *restrict contents, const char filename[restrict static 1]);
+// mmap() allocates a whole page at a time, I recommend setting this to a
+// multiple of one of your system's supported page sizes.
+static constexpr uint16_t MESSAGE_SIZE = 4096;
 
-void receive_data(char *restrict contents, const char filename[restrict static 1]);
+// Can be called from any process to write at filename.
+// Once sent, the process will block until the file is read.
+// Once read, the data is deleted.
+// Behaviour if the file is already filled is undefined, or rather it is defined as 'will not work'.
+// Use sockets or something if you actually want secure communication
+void send_data(const char *contents, const char filename[restrict static 1]);
+
+// Thread-blocking, will await for data to be written somewhere before reading
+// it.
+void receive_data(char contents[restrict static MESSAGE_SIZE], const char filename[restrict static 1]);
 
 #endif // LIBMESSAGING_H_
